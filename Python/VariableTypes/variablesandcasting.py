5118c1a528742f1f39cd2e35872f6c858e4aca29
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2019-11-02 15:22:17 -0300

    Iniciando com python

---
+++ Python/VariableTypes/variablesandcasting.py
@@ -0,0 +1,38 @@
+#declarando uma variavel
+
+#---------------Types of variables ------------------
+
+#variavel recebendo string
+stringvar="declarei" #variavel stringvar que recebe uma string chamada declarei
+
+#variavel recebendo inteiro
+numint = 7
+
+#variavel recebendo string float 2.7
+varstring ="2.7"
+
+#variavel recebendo string float 1.0
+varstringnum="1.0"
+
+#variavel recebendo string int 1
+varotherstringnum="1"
+
+#variavel recebendo boolean
+varbooleanExampleTrue = True
+varbooleanExampleFalse = False
+
+#variavel recebendo null
+varnull = null
+
+
+#----------------------To casting ------------------
+
+#transformando a variavel varstring em float e guardando na nova variavel
+#varfloatcast
+varfloatcast = float(varstring)
+
+#varintstringnum= int(varstringnum)#assimvaidarerro
+varstringnumbertonum= int(varotherstringnum)
+#varcasttostring= string(numint)
+
+
